6a784ac2c004c00ec3baa900291f73ccd4f0beba
Davide Agostini <d.agostini@synapseslab.com>
2023-03-15 10:13:58 +0100

    update copyright

--- demo-app/app/src/main/java/com/synapseslab/bluegpssdkdemo/resources/SearchResourceViewModel.kt
+++ demo-app/app/src/main/java/com/synapseslab/bluegpssdkdemo/resources/SearchResourceViewModel.kt
@@ -1,9 +1,16 @@
 /*
- * (c) Copyright 2021. All rights reserved by Synapses S.r.l.s.
- * https://www.synapseslab.com/
+ * Copyright (c) 2023 Synapses s.r.l.s. All rights reserved.
  *
- * Created by Davide Agostini on 30/11/21, 14:41.
- * Last modified 30/11/21, 14:41
+ * Licensed under the Apache License.
+ * You may obtain a copy of the License at
+ *
+ * https://github.com/synapseslab/android-bluegps-sdk-demoapp/blob/main/LICENSE.md
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.synapseslab.bluegpssdkdemo.resources
--- demo-app/app/src/main/java/com/synapseslab/bluegpssdkdemo/search_object/ObjectDetailViewModel.kt
+++ demo-app/app/src/main/java/com/synapseslab/bluegpssdkdemo/search_object/ObjectDetailViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2023 Synapses s.r.l.s. All rights reserved.
+ *
+ * Licensed under the Apache License.
+ * You may obtain a copy of the License at
+ *
+ * https://github.com/synapseslab/android-bluegps-sdk-demoapp/blob/main/LICENSE.md
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.synapseslab.bluegpssdkdemo.search_object
 
 import androidx.lifecycle.MutableLiveData
